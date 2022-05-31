4a13576511c7e11cba47502c8c43053b4ca1e586
preva978 <prevotadrian@gmail.com>
2022-05-31 10:38:05 +0200

    commit

--- src/datas/Experiences.js
+++ src/datas/Experiences.js
@@ -1,9 +1,9 @@
 export const dataExperiences = [
     {
       id: 1,
-      title: "Développeur Web - Full Stack",
-      date: "12/03/2017 - 21/08/2020",
-      location: "Paris 13ème",
+      title: "",
+      date: "",
+      location: "",
       text: `Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aperiam facilis at iure culpa inventore consequatur.`,
       missions: [
         {
@@ -23,8 +23,8 @@ export const dataExperiences = [
     {
       id: 2,
       title: "Développeur Web",
-      date: "23/01/2012 - 05/06/2016",
-      location: "Montpellier",
+      date: "",
+      location: "",
       text: `Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aperiam facilis at iure culpa inventore consequatur.`,
       missions: [
         {
@@ -43,9 +43,9 @@ export const dataExperiences = [
     },
     {
       id: 3,
-      title: "Intégrateur Front End",
-      date: "06/06/2008 - 21/02/2011",
-      location: "Lyon",
+      title: "",
+      date: "",
+      location: "",
       text: `Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aperiam facilis at iure culpa inventore consequatur.`,
       missions: [
         {
